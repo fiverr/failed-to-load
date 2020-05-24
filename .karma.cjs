c9f474b50aa9db3bfebbda1dd62093868e2dbd43
omrilotan <omri@hamadgera.com>
2020-05-24 12:57:11 +0300

    include config files in lint

--- .karma.cjs
+++ .karma.cjs
@@ -1,24 +1,24 @@
 const { join } = require('path');
-const { output: { file } } = require('./.rollup.cjs')
+const { output: { file } } = require('./.rollup.cjs');
 
 module.exports = (config) => {
-  const { LOG_INFO: logLevel } = config;
+    const { LOG_INFO: logLevel } = config;
 
-  config.set({
-    browsers: [ 'Chrome' ],
-    frameworks: [ 'mocha' ],
-    port: 9876,
-    logLevel,
-    singleRun: true,
-    concurrency: 1,
-    hooks : [
-        'karma-chrome-launcher',
-        'karma-mocha',
-        'karma-mocha-reporter'
-    ],
-    reporters: ['mocha'],
-    basePath: __dirname,
-    customContextFile: join(__dirname, 'integration/context.html'),
-    files: [ file ]
-  })
-}
+    config.set({
+        browsers: [ 'Chrome' ],
+        frameworks: [ 'mocha' ],
+        port: 9876,
+        logLevel,
+        singleRun: true,
+        concurrency: 1,
+        hooks : [
+            'karma-chrome-launcher',
+            'karma-mocha',
+            'karma-mocha-reporter'
+        ],
+        reporters: [ 'mocha' ],
+        basePath: __dirname,
+        customContextFile: join(__dirname, 'integration/context.html'),
+        files: [ file ]
+    });
+};
--- .rollup.cjs
+++ .rollup.cjs
@@ -2,13 +2,13 @@ const commonjs = require('@rollup/plugin-commonjs');
 const { default: resolve } = require('@rollup/plugin-node-resolve');
 
 module.exports = {
-  input: 'integration/test.js',
-  output: {
-    file: 'integration/.compiled.js',
-    format: 'iife'
-  },
-  plugins: [
-    commonjs(),
-    resolve()
-  ]
-}
+    input: 'integration/test.js',
+    output: {
+        file: 'integration/.compiled.js',
+        format: 'iife'
+    },
+    plugins: [
+        commonjs(),
+        resolve()
+    ]
+};
