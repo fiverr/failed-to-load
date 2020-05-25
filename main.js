65e6ca920fa7d87be1c7a02b458f844def01d27f
omrilotan <omri@hamadgera.com>
2020-05-25 21:02:41 +0300

    Fix hard-coded [object Object] string (thanks!)

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
+/**
+ * This is a stubs module for nodejs runtime.
+ * The package is built for browser usage only, see package "browser" entry
+ */
 module.exports.check = () => [];
 module.exports.register = () => null;
